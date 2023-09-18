03564448328fea6d25ecc4e3319fb61ab93c820a
FRZi7 <pt.firoz729@gmail.com>
2023-09-18 19:28:21 +0530

    changes

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const admincontroller = require("../controllers/adminccontroller")
+const admincontroller = require("../controllers/admincontroller")
 const authMiddlewares = require("../middlewares/authAdminMiddleware")
 
 
